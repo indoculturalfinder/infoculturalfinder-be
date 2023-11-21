0984a20ea726de2d376c65fec3b8ed51b1449b31
Muhammad Ilman Aqilaa <ilmanaqilaa2@gmail.com>
2023-11-21 11:32:39 +0700

    add(categorie.php): Add categories model

---
+++ app/Models/Categorie.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Categorie extends Model
+{
+    use HasFactory;
+
+    protected $table = "categories";
+    protected $primarykey = "id";
+    protected $fillable = [
+        'name',
+    ];
+
+    public function cultures(){
+        return $this->hasMany(Culture::class, 'category_id', 'id');
+    }
+}
