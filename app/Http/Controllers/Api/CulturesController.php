4d7cf1d130aa5fd9c8f1da82b89536ee037818c6
Muhammad Ilman Aqilaa <ilmanaqilaa2@gmail.com>
2023-11-22 14:28:21 +0700

    add(CulturesController.php): add getByProvinces for cultures endpoint

--- app/Http/Controllers/Api/CulturesController.php
+++ app/Http/Controllers/Api/CulturesController.php
@@ -93,6 +93,30 @@ class CulturesController extends Controller
         }
     }
 
+    public function searchByProvince(string $name)
+    {
+        $data = Culture::join('provinces', 'cultures.province_id', '=', 'provinces.id')
+            ->join('categories', 'cultures.category_id', '=', 'categories.id')
+            ->select('cultures.id', 'cultures.province_id', 'provinces.name as province_name', 'cultures.category_id', 'categories.name as category_name', 'cultures.name', 'cultures.img', 'cultures.video', 'cultures.desc')
+            ->where('provinces.name', 'like', "%$name%")
+            ->orderBy('cultures.id', 'asc')
+            ->get();
+
+        if ($data->isNotEmpty()) {
+            return response()->json([
+                'status' => true,
+                'message' => 'Data Ditemukan',
+                'cultures' => $data,
+            ], 200);
+        } else {
+            return response()->json([
+                'status' => false,
+                'message' => 'Data tidak ditemukan',
+                'cultures' => $data,
+            ], 404);
+        }
+    }
+
     /**
      * Update the specified resource in storage.
      */
