1e6678efd4863aa8f37f3f397f9fc552e3530c33
Muhammad Ilman Aqilaa <ilmanaqilaa2@gmail.com>
2023-11-22 13:07:06 +0700

    add(api.php): add route endpoint for all function categories endpoint

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// Categories
+Route::get('categories', [CategoriesController::class, 'index']);
+Route::get('categories/{id}', [CategoriesController::class, 'show']);
+Route::post('categories', [CategoriesController::class, 'store']);
+Route::put('categories/{id}', [CategoriesController::class, 'update']);
+Route::delete('categories/{id}', [CategoriesController::class, 'destroy']);
