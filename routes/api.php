4b5aa3122ec12d3af043e9c16456cb807d15e213
Muhammad Ilman Aqilaa <ilmanaqilaa2@gmail.com>
2023-11-22 15:15:18 +0700

    add(api.php): import controller to route for all endpoint

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Http\Request;
+use App\Http\Controllers\Api\Tarian;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\CulturesController;
+use App\Http\Controllers\Api\ProvincesController;
+use App\Http\Controllers\Api\CategoriesController;
 
 /*
 |--------------------------------------------------------------------------
