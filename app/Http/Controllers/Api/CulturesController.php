3531b1936b4e543705cd4cf5eccbe2dc9b61f8dc
Muhammad Ilman Aqilaa <ilmanaqilaa2@gmail.com>
2023-11-22 13:35:49 +0700

    add(CulturesController): add function getAll, getByID for cultures endpoint

--- app/Http/Controllers/Api/CulturesController.php
+++ app/Http/Controllers/Api/CulturesController.php
@@ -2,8 +2,9 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Controllers\Controller;
+use App\Models\Culture;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class CulturesController extends Controller
 {
@@ -12,7 +13,17 @@ class CulturesController extends Controller
      */
     public function index()
     {
-        //
+        $data = Culture::join('provinces', 'cultures.province_id', '=', 'provinces.id')
+        ->join('categories', 'cultures.category_id', '=', 'categories.id')
+        ->select('cultures.id', 'cultures.province_id', 'provinces.name as province_name', 'cultures.category_id', 'categories.name as category_name', 'cultures.name', 'cultures.img', 'cultures.video', 'cultures.desc')
+        ->orderBy('cultures.id', 'asc')
+        ->get();
+
+        return response()->json([
+        'status' => true,
+        'message' => 'Data ditemukan',
+        'Cultures' => $data
+        ], 200);
     }
 
     /**
@@ -28,7 +39,25 @@ class CulturesController extends Controller
      */
     public function show(string $id)
     {
-        //
+        $data = Culture::join('provinces', 'cultures.province_id', '=', 'provinces.id')
+        ->join('categories', 'cultures.category_id', '=', 'categories.id')
+        ->select('cultures.id', 'cultures.province_id', 'provinces.name as province_name', 'cultures.category_id', 'categories.name as category_name', 'cultures.name', 'cultures.img', 'cultures.video', 'cultures.desc')
+        ->orderBy('cultures.id', 'asc')
+        ->find($id);
+
+        if ($data) {
+            return response()->json([
+                'status'=>true,
+                'message'=>'Data Ditemukan',
+                'cultures'=>$data,
+            ], 200);
+        }else{
+            return response()->json([
+                'status' => false,
+                'message' => 'Data tidak ditemukan',
+                'cultures' => $data
+            ], 404);
+        }
     }
 
     /**
