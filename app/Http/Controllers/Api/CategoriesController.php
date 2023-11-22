185262aa1c76573ec26a467962d8ef3549d7fd33
Muhammad Ilman Aqilaa <ilmanaqilaa2@gmail.com>
2023-11-22 13:05:02 +0700

    add()CategoriesController.php): add function method POST, PUT for categories endpoint

--- app/Http/Controllers/Api/CategoriesController.php
+++ app/Http/Controllers/Api/CategoriesController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Models\Categorie;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Validator;
 
 class CategoriesController extends Controller
 {
@@ -27,7 +28,33 @@ class CategoriesController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $dataCategories = new Categorie;
+
+        $rules = [
+            'name' => 'required',
+            'img' => 'required',
+            'desc' => 'required'
+        ];
+
+        $validator = Validator::make($request->all(), $rules);
+        if ($validator->fails()) {
+            return response()->json([
+                'status'=>false,
+                'massage'=>'Gagal memasukkan data',
+                'data'=>$validator->errors()
+            ], 400);
+        }
+
+        $dataCategories->name = $request->name;
+        $dataCategories->img = $request->img;
+        $dataCategories->desc = $request->desc;
+
+        $dataCategories->save();
+
+        return response()->json([
+            'status' => true,
+            'message' => 'Sukses memasukan data'
+        ],201);
     }
 
     /**
@@ -56,7 +83,40 @@ class CategoriesController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        $dataCategories = Categorie::find($id);
+
+        if (empty($dataCategories)) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Data tidak ditemukan'
+            ], 404);
+        }
+
+        $rules = [
+            'name' => 'required',
+            'img' => 'required',
+            'desc' => 'required'
+        ];
+
+        $validator = Validator::make($request->all(), $rules);
+        if ($validator->fails()) {
+            return response()->json([
+                'status'=>false,
+                'massage'=>'Gagal update data',
+                'data'=>$validator->errors()
+            ], 400);
+        }
+
+        $dataCategories->name = $request->name;
+        $dataCategories->img = $request->img;
+        $dataCategories->desc = $request->desc;
+
+        $dataCategories->save();
+
+        return response()->json([
+            'status' => true,
+            'message' => 'Sukses update data'
+        ],201);
     }
 
     /**
